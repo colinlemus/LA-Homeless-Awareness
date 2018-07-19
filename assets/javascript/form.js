ccdd1884965a18cbb788e01e497153fa0112770f
Broodis <j.porter712@gmail.com>
2018-07-18 20:53:03 -0700

    carousel

--- assets/javascript/form.js
+++ assets/javascript/form.js
@@ -13,57 +13,57 @@ var $victimCrime = $("#victimCrime");
 var $submitButton = $("#submitButton");
 var totalRiskScore;
 
-$submitButton.on("click", function(event) {
-	event.preventDefault();
-    if($militaryService.val() == 0) {
-        totalRiskScore++;
-	}
-
-    if($areaHelp.val() == 0) {
-        totalRiskScore++;
-	}
-	
-    if($healthIssues.val() == 0) {
-        totalRiskScore++;
-	}
-	
-    if($fosterCare.val() == 0) {
-        totalRiskScore++;
-	}
-	
-    if($victimCrime.val() == 0) {
+$submitButton.on("click", function (event) {
+    event.preventDefault();
+    if ($militaryService.val() == 0) {
         totalRiskScore++;
     }
 
-    if($PTSD.val() == 0) {
-    	totalRiskScore = totalRiskScore + 2;
+    if ($areaHelp.val() == 0) {
+        totalRiskScore++;
     }
 
-    if($debtAmount.val() == 0) {
+    if ($healthIssues.val() == 0) {
+        totalRiskScore++;
+    }
+
+    if ($fosterCare.val() == 0) {
+        totalRiskScore++;
+    }
+
+    if ($victimCrime.val() == 0) {
+        totalRiskScore++;
+    }
+
+    if ($PTSD.val() == 0) {
         totalRiskScore = totalRiskScore + 2;
     }
 
-    if($rent.val() == 0) {
+    if ($debtAmount.val() == 0) {
         totalRiskScore = totalRiskScore + 2;
     }
 
-    if($addiction.val() == 0) {
+    if ($rent.val() == 0) {
         totalRiskScore = totalRiskScore + 2;
     }
 
-    if($jobLoss.val() == 0) {
+    if ($addiction.val() == 0) {
         totalRiskScore = totalRiskScore + 2;
     }
 
-    if($mentalHealthIssues.val() == 0) {
+    if ($jobLoss.val() == 0) {
         totalRiskScore = totalRiskScore + 2;
     }
 
-    if($homeless.val() == 0) {
+    if ($mentalHealthIssues.val() == 0) {
         totalRiskScore = totalRiskScore + 2;
-	}
+    }
 
-	pullInformation();
+    if ($homeless.val() == 0) {
+        totalRiskScore = totalRiskScore + 2;
+    }
+
+    pullInformation();
 });
 
 function onStart() {
